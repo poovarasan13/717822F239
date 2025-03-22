2fc4f90e9fc43b97f7237d1a457831f1f1cf9eb4
Poovarasan <poovarasan936161@gmail.com>
2025-03-22 11:44:24 +0530

    update program2

---
+++ src/Components/MainPage.js
@@ -0,0 +1,95 @@
+import React, { Fragment, useState } from 'react';
+import { Container, Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import axios from 'axios'; 
+const MainPage = () => {
+  const [data, setData] = useState(null); 
+  const [error, setError] = useState(null); 
+  const [type, setType] = useState('fibo');
+  const [details, setDetails] = useState({
+    windowPrevState: [], 
+    windowCurrState: [], 
+    numbers: [], 
+    avg: 0, 
+  });
+  const apiFetch = async (selectedType) => {
+    try {
+      const url = `http://localhost:9000/data`; 
+      const response = await axios.get(url); 
+      const result = response.data.data; 
+
+      setData(result); 
+      console.log(result);
+
+      const newNumbers = result.numbers.filter((n) => !details.windowCurrState.includes(n));
+
+      let updatedWindow = [...details.windowCurrState, ...newNumbers];
+
+      if (updatedWindow.length > 10) {
+        const overflow = updatedWindow.length - 10; 
+        updatedWindow = updatedWindow.slice(overflow);
+      }
+
+   
+      setDetails({
+        windowPrevState: details.windowCurrState, 
+        windowCurrState: updatedWindow, 
+        numbers: result.numbers, 
+        avg: updatedWindow.reduce((a, b) => a + b, 0) 
+      });
+
+      setError(null); 
+    } catch (err) {
+      setError(err.message); 
+      console.error(err.message);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    apiFetch(type); 
+  };
+
+  return (
+    <Fragment>
+         <Container>
+        <h1>Average Calculator Microservice</h1>
+
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label for="numberType">Select Number Type</Label>
+            <Input
+              type="select"
+              name="numberType"
+              id="numberType"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            >
+              <option value="primes">Prime Numbers</option>
+              <option value="fibo">Fibonacci Numbers</option>
+              <option value="even">Even Numbers</option>
+              <option value="rand">Random Numbers</option>
+            </Input>
+          </FormGroup>
+          <Button color="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+
+        {error && <p style={{ color: 'red' }}>Error: {error}</p>}
+
+     
+        {data && (
+          <div style={{ marginTop: '20px' }}>
+            <h2>API Response</h2>
+            <pre>{JSON.stringify(data,null,4)}</pre>
+
+            <h2>Details</h2>
+            <pre>{JSON.stringify(details,null,4)}</pre>
+          </div>
+        )}
+      </Container>
+    </Fragment>
+  )
+}
+
+export default MainPage
